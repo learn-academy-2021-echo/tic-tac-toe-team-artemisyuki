9daab980d776d39e66e957d85725c3dcd93de937
kirkkwang <k3wang@gmail.com>
2022-01-05 09:49:02 -0800

    created 3x3 grid on the main page

--- src/App.js
+++ src/App.js
@@ -1,22 +1,26 @@
-import React, { Component } from 'react'
-import Square from './components/Square'
-import './App.css'
+import React, { Component } from "react";
+import Square from "./components/Square";
+import "./App.css";
 
-class App extends Component{
-  constructor(props){
-    super(props)
+class App extends Component {
+  constructor(props) {
+    super(props);
     this.state = {
-      squares: [0, 0, 0, 0, 0, 0, 0, 0, 0]
-    }
+      squares: [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    };
   }
 
-  render(){
-    return(
+  render() {
+    return (
       <>
-        <h1>Tic Tac Toe</h1>
-        <Square />
+        <h1 className="header">Tic Tac Toe</h1>
+        <div className="gameboard">
+          {this.state.squares.map((value, idx) => (
+            <Square value={value} key={idx} idx={idx} />
+          ))}
+        </div>
       </>
-    )
+    );
   }
 }
-export default App
+export default App;
--- src/components/Square.js
+++ src/components/Square.js
@@ -1,12 +1,12 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
-class Square extends Component{
-  render(){
-    return(
+class Square extends Component {
+  render() {
+    return (
       <>
-        <div className="square"></div>
+        <div className="square">{this.props.idx}</div>
       </>
-    )
+    );
   }
 }
-export default Square
+export default Square;
