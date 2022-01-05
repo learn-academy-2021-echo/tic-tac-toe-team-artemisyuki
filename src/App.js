c0b36521aff6b79444e206641f154ff251714e7a
kirkkwang <k3wang@gmail.com>
2022-01-05 10:34:21 -0800

    created funcitonality for my partner to be able to click on a square after me and see their mark

--- src/App.js
+++ src/App.js
@@ -6,17 +6,36 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      squares: [0, 0, 0, 0, 0, 0, 0, 0, 0],
+      squares: ["", "", "", "", "", "", "", "", ""],
+      player: "Player 1's Turn",
     };
   }
+
+  handleTurns = (index) => {
+    const { squares, player } = this.state;
+    player === "Player 1's Turn"
+      ? (squares[index] = "❌")
+      : (squares[index] = "⭕️");
+    this.setState({
+      squares: squares,
+      player:
+        player === "Player 1's Turn" ? "Player 2's Turn" : "Player 1's Turn",
+    });
+  };
 
   render() {
     return (
       <>
         <h1 className="header">Tic Tac Toe</h1>
+        <h3 className="turns">{this.state.player}</h3>
         <div className="gameboard">
           {this.state.squares.map((value, idx) => (
-            <Square value={value} key={idx} idx={idx} />
+            <Square
+              value={value}
+              key={idx}
+              idx={idx}
+              handleTurns={this.handleTurns}
+            />
           ))}
         </div>
       </>
